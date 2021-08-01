8b005526339042a28600f08580dde783afba8713
Paul Wrubel <pwrubel7@gmail.com>
2021-08-01 16:27:20 -0500

    routing fix

--- routing.go
+++ routing.go
@@ -11,20 +11,18 @@ func listenAndServe(log *logrus.Logger, appData *appData) {
 	router := mux.NewRouter().PathPrefix("/v1").Subrouter()
 
 	// /activity
-	activitiesRouter := router.Path("/activities").Subrouter()
-	activitiesRouter.Path("/{id}").HandlerFunc(getActivitiesGetHandlerFunc(log, appData)).Methods("GET")
-	activitiesRouter.Path("").HandlerFunc(getActivitiesGetAllHandlerFunc(log, appData)).Methods("GET")
-	activitiesRouter.Path("").HandlerFunc(getActivitiesPostHandlerFunc(log, appData)).Methods("POST")
-	activitiesRouter.Path("/{id}").HandlerFunc(getActivitiesPutHandlerFunc(log, appData)).Methods("PUT")
-	activitiesRouter.Path("/{id}").HandlerFunc(getActivitiesDeleteHandlerFunc(log, appData)).Methods("DELETE")
+	router.Path("/activity/{id}").HandlerFunc(getActivitiesGetHandlerFunc(log, appData)).Methods("GET")
+	router.Path("/activity").HandlerFunc(getActivitiesGetAllHandlerFunc(log, appData)).Methods("GET")
+	router.Path("/activity").HandlerFunc(getActivitiesPostHandlerFunc(log, appData)).Methods("POST")
+	router.Path("/activity/{id}").HandlerFunc(getActivitiesPutHandlerFunc(log, appData)).Methods("PUT")
+	router.Path("/activity/{id}").HandlerFunc(getActivitiesDeleteHandlerFunc(log, appData)).Methods("DELETE")
 
 	// /workouts
-	workoutsRouter := router.Path("/workouts").Subrouter()
-	workoutsRouter.Path("/{id}").HandlerFunc(getWorkoutsGetHandlerFunc(log, appData)).Methods("GET")
-	workoutsRouter.Path("").HandlerFunc(getWorkoutsGetAllHandlerFunc(log, appData)).Methods("GET")
-	workoutsRouter.Path("").HandlerFunc(getWorkoutsPostHandlerFunc(log, appData)).Methods("POST")
-	workoutsRouter.Path("/{id}").HandlerFunc(getWorkoutsPutHandlerFunc(log, appData)).Methods("PUT")
-	workoutsRouter.Path("/{id}").HandlerFunc(getWorkoutsDeleteHandlerFunc(log, appData)).Methods("DELETE")
+	router.Path("/workouts/{id}").HandlerFunc(getWorkoutsGetHandlerFunc(log, appData)).Methods("GET")
+	router.Path("/workouts").HandlerFunc(getWorkoutsGetAllHandlerFunc(log, appData)).Methods("GET")
+	router.Path("/workouts").HandlerFunc(getWorkoutsPostHandlerFunc(log, appData)).Methods("POST")
+	router.Path("/workouts/{id}").HandlerFunc(getWorkoutsPutHandlerFunc(log, appData)).Methods("PUT")
+	router.Path("/workouts/{id}").HandlerFunc(getWorkoutsDeleteHandlerFunc(log, appData)).Methods("DELETE")
 
 	go func() {
 		if err := http.ListenAndServe(":8080", router); err != nil {
